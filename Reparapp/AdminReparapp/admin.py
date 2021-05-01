b87a2ba61277e75c5d5bb1b6df7cd197fd222672
Cristian <cmacostap@uqvirtual.edu.co>
2021-05-01 18:16:48 -0500

    make it better

--- Reparapp/AdminReparapp/admin.py
+++ Reparapp/AdminReparapp/admin.py
@@ -3,12 +3,12 @@ from .models import Agente,TecnicoEspecialista,Operador, Usuario
 from AgenteReparapp.models import Orden,Cliente,Producto
 from TecnicoReparapp.models import Taller, CallCenter
 # Register your models here.
-admin.site.register(Agente)
-admin.site.register(Operador)
-admin.site.register(TecnicoEspecialista)
-admin.site.register(Orden)
-admin.site.register(Cliente)
-admin.site.register(Usuario)
+# admin.site.register(Agente)
+# admin.site.register(Operador)
+# admin.site.register(TecnicoEspecialista)
+# admin.site.register(Orden)
+# admin.site.register(Cliente)
+# admin.site.register(Usuario)
 admin.site.register(Taller)
 admin.site.register(CallCenter)
-admin.site.register(Producto)
+# admin.site.register(Producto)
--- Reparapp/Reparapp/settings.py
+++ Reparapp/Reparapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^79su5dy%4!k*#-qg^&57@d&7z9)uujiar!x2)hn5ro7lzelr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
